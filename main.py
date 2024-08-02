6f8a674a774b6c20bd7504e6e8f5a006d4cd4e16
yashpal-paliwal <66688816+yashpal-paliwal@users.noreply.github.com>
2024-08-02 19:12:43 +0000

    testing code of python

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 def hello_world():
     return "Hello, GitHub Actions!"
-
-if __name__ == "__main__":
-    print(hello_world())
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from demo_github_app import hello_world
+from main import hello_world
 
 def test_hello_world():
     assert hello_world() == "Hello, GitHub Actions!"
